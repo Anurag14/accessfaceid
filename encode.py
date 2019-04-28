b3479c29cf0724f78589d138bf616c5f3cf1ddc6
Anurag <tellanurag95@gmail.com>
2019-04-28 20:35:15 +0530

    encoding update for DL

--- encode.py
+++ encode.py
@@ -1,10 +1,12 @@
 import os
 import cv2
-import face_recognition
+import warnings
+from training.vggface import vggface
+from training.utils import preprocess_image
 import numpy as np
 from tqdm import tqdm
-from collections import defaultdict
-from imutils.video import VideoStream    
+from faced import FaceDetector
+from keras.models import Model
 def process_and_encode(dataset):
     print("[LOG] Collecting images ...")
     images = []
@@ -16,22 +18,21 @@ def process_and_encode(dataset):
     known_encodings = []
     known_names = []
     print("[LOG] Encoding faces ...")
-
+    model=vggface()
+    vgg_face_descriptor = Model(inputs=model.layers[0].input, outputs=model.layers[-2].output)
+    face_detector = FaceDetector()
     for image_path in tqdm(images):
-        # Load image
         image = cv2.imread(image_path)
-        # Convert it from BGR to RGB
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-     
-        # detect face in the image and get its location (square boxes coordinates)
-        boxes = face_recognition.face_locations(image, model='hog')
-
-        # Encode the face into a 128-d embeddings vector
-        encoding = face_recognition.face_encodings(image, boxes)
-
+        boxes = face_detector.predict(image)
+        if(boxes==[]):
+            warnings.warn('system could not detect face in this image %s'%(image_path))
+            continue
+        (x,y,w,h,prob)=boxes[0]
+        #TODO align faces 
+        encoding = vgg_face_descriptor.predict(preprocess_image(image[int(x - w/2):int(x + w/2),int(y - h/2):int(y + h/2)]))[0,:]
         # the person's name is the name of the folder where the image comes from
         name = image_path.split(os.path.sep)[-2]
-
         if len(encoding) > 0 : 
             known_encodings.append(encoding[0])
             known_names.append(name)
