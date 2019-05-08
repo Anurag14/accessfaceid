1408f688f5cd88a73e14cb8c54c60307c1ce41c0
Anurag <tellanurag95@gmail.com>
2019-05-08 11:21:44 +0530

    config added

--- training/utils.py
+++ training/utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from configs import configs
 from keras.preprocessing.image import img_to_array
 from keras.applications.imagenet_utils import preprocess_input
 from keras.preprocessing import image
@@ -60,31 +61,17 @@ def execute_alignment(img,preds):
     rotated = cv2.warpAffine(img, M, (width, height), flags=cv2.INTER_CUBIC)
     return rotated
 """
-    VGG FACE MODEL utils
+    INSIGHT FACE MODEL utils
 """
-def preprocess_image(img):
-    img = cv2.resize(img,(224, 224))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    return img
-def findCosineSimilarity(source_representation, test_representation):
-    a = np.matmul(np.transpose(source_representation), test_representation)
-    b = np.sum(np.multiply(source_representation, source_representation))
-    c = np.sum(np.multiply(test_representation, test_representation))
-    return 1 - (a / (np.sqrt(b) * np.sqrt(c)))
+def face_distance(face_encodings, face_to_compare):
+    if len(face_encodings) == 0:
+        return np.empty((0))
+    face_dist_value = np.linalg.norm(face_encodings - face_to_compare, axis=1)
+    print('[Face Services | face_distance] Distance between two faces is {}'.format(face_dist_value))
+    return face_dist_value
 
-def findEuclideanDistance(source_representation, test_representation):
-    euclidean_distance = source_representation - test_representation
-    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
-    euclidean_distance = np.sqrt(euclidean_distance)
-    return euclidean_distance
 
-def verifyFace(vgg_face_descriptor,img1, img2,epsilon=0.4):
-    img1_representation = vgg_face_descriptor.predict(preprocess_image(img1))[0,:]
-    img2_representation = vgg_face_descriptor.predict(preprocess_image(img2))[0,:]
-    
-    cosine_similarity = findCosineSimilarity(img1_representation, img2_representation)
-    #euclidean_distance = findEuclideanDistance(img1_representation, img2_representation)
-    return True if (cosine_similarity < epsilon) else False
-
+def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=configs.face_similarity_threshold):
+    true_list = list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+    similar_indx = list(np.where(true_list)[0])
+    return similar_indx
