aa53806c4204257aca0836a0f2989c0a47b21deb
Anurag <tellanurag95@gmail.com>
2019-05-21 12:08:46 +0530

    fix for demo

--- livestream/newmain.py
+++ livestream/newmain.py
@@ -31,12 +31,12 @@ class Demo(camera_server.CameraServer):
         self.face_tracker.process(frame)
         _faces = self.face_tracker.get_faces()
 
-        # Uncomment below to visualize face
+        # Step2. Get all the face locations aka bounding boxes
         _faces_loc = self.face_tracker.get_faces_loc()
-        self._viz_faces(_faces_loc, frame)
 
-        # Step2. For each face, get the cropped face area, feeding it to face describer (insightface) to get 512-D Feature Embedding
+        # Step3. For each face, get the cropped face area, feeding it to face describer (insightface) to get 512-D Feature Embedding
         _face_descriptions = []
+        _names = []
         _num_faces = len(_faces)
         if _num_faces == 0:
             return
@@ -46,20 +46,25 @@ class Demo(camera_server.CameraServer):
             _face_description = self.face_describer.inference(_data_feed)[0][0]
             _face_descriptions.append(_face_description)
 
-            # Step3. For each face, check whether there are similar faces and if not save it to db.
+            # Step4. For each face, check whether get its name
             # Below naive and verbose implementation is to tutor you how this work
-            _similar_faces = self.face_db.get_similar_faces(_face_description)
-            if len(_similar_faces) == 0 or len(self.face_db.faces) == 0:
-                self.face_db.add_face(face_img=_face, face_description=_face_description)
+            _similar_face_name = self.face_db.get_similar_faces(_face_description)
+            _names.append(_similar_face_name)
+        #Step5. Visualize all the faces in the frame
+        self._viz_faces(_face_loc,_names, frame)
         print('[Demo] -----------------------------------------------------------')
 
-    def _viz_faces(self, faces_loc, frame):
-        for _face_loc in faces_loc:
-            x1 = int(_face_loc[0] * self.face_tracker.cam_w)
-            y1 = int(_face_loc[1] * self.face_tracker.cam_h)
-            x2 = int(_face_loc[2] * self.face_tracker.cam_w)
-            y2 = int(_face_loc[3] * self.face_tracker.cam_h)
+    def _viz_faces(self, faces_loc, names, frame):
+        for i in range(len(names)):
+            x1 = int(faces_loc[i][0] * self.face_tracker.cam_w)
+            y1 = int(faces_loc[i][1] * self.face_tracker.cam_h)
+            x2 = int(faces_loc[i][2] * self.face_tracker.cam_w)
+            y2 = int(faces_loc[i][3] * self.face_tracker.cam_h)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            # Draw a filled rectangle below bounding box for writing name
+            cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 0, 255), cv2.FILLED)
+            # Write name
+            cv2.putText(img,names[i],(x1 + 6, y2 - 6),cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 255, 255), 1)
         cv2.imshow('faces', frame)
         cv2.waitKey(1)
 
